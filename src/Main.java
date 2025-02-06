6498440413a6c8458967a7205b617fd5164cbdd8
Michael J Cash 12th CCHS <100093@SWCSD.US>
2025-02-06 14:06:34 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        HashMap<String, String> capitalCities = new HashMap<String, String>();
+        System.out.println("Please type the city you would like to enter");
+        capitalCities.put(input.nextLine(), input.nextLine());
+        capitalCities.put(input.nextLine(), input.nextLine());
+        capitalCities.put(input.nextLine(), input.nextLine());
+        capitalCities.put(input.nextLine(), input.nextLine());
+        System.out.println(capitalCities);
+        System.out.println("Please type the city you would like to enter");
+        String userInput = input.nextLine();
+        String STOP = "END";
+
+        while(!userInput.equals(STOP)){
+
+            System.out.println("Please enter another city, when finished type END");
+            input.nextLine();
+            if (userInput.equals(STOP)) {
+                break;
+            }
+        }
+    }
+
+}
